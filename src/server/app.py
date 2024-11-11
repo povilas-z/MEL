39198d309e666f3ce76f3e049962373abe9f0904
Povilas Zinkevicius <pz2036@nyu.edu>
2024-11-11 01:32:40 -0500

    Update app.py

--- src/server/app.py
+++ src/server/app.py
@@ -9,9 +9,7 @@ def upload_file():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
     
-    # TODO: Here, you'll later add the code to process the image through your ML model.
-    # For now, we'll just return a placeholder message.
     return jsonify({'message': 'File received!'})
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the server in debug mode for development
+    app.run(debug=True) 
